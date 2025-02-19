a0f944740c095792fd79fffcc3511cb9f7680cf9
sabermodirian <sabermodirian@gmail.com>
2025-02-19 12:35:28 +0330

    Add Docstring to FBV and CBV  to show the index page

--- core/blog/views.py
+++ core/blog/views.py
@@ -6,6 +6,7 @@ from .models import Post  # noqa: F401
 # Create your views here.
 
 def indexView(request):
+    ''' this is a function based view to show the index page'''
     title="FBV_Index"
     context={ "title":title }
        
@@ -13,6 +14,7 @@ def indexView(request):
 
 #from django.views.generic.base import TemplateView
 class indexView(TemplateView):  # noqa: F811
+    ''' this is a class based view to show the index page'''
     template_name = "index.html"
     def  get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
